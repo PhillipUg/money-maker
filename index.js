436b8d7e427ca36bb41d2a83d24ba93d5342785c
Phillip Musiime <phillipmusiime@gmail.com>
2020-11-05 19:00:04 +0300

    rem sen da

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const axios = require('axios');
+// const axios = require('axios');
 
-run().catch(err => console.log(err));
+// run().catch(err => console.log(err));
 
-async function run() {
-  const url = 'https://hooks.slack.com/services/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';
-  const res = await axios.post(url, {
-    text: 'Hello Phillip, You\'ve got money to make!',
-    icon_emoji: ':+1:'
-  });
+// async function run() {
+//   const url = '';
+//   const res = await axios.post(url, {
+//     text: 'Hello, You\'ve got money to make!',
+//   });
 
-  console.log('Done', res.data);
-}
+//   console.log('Done', res.data);
+// }
